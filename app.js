e6d25294ee70e4e196d0b5c49fa0fad821690a57
alphaprime7 <awesome.tingwei@outlook.com>
2024-03-29 16:57:38 -0500

    Window object prober

---
+++ app.js
@@ -0,0 +1,8 @@
+//app.js is shared by 2 html documents.
+
+//WINDOW_TEST.HTML
+function changepage(){
+    window.location="redirect.html";
+}
+//console.log(window.document.getElementById('htmlcode')) //document is a property = html
+
